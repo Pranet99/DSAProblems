0ba1d99d69f721e3cb280ae9ad03e47eb8a15d1f
Pranet99 <praneethv65@gmail.com>
2024-09-29 21:43:17 +0530

    adding new program

---
+++ Programms/PrintNumbersusingrecursion.java
@@ -0,0 +1,23 @@
+public class PrintNumbersusingrecursion {
+  public static void printArray(int[] a,int i,int l){
+    if(i>a.length-1){
+        return;
+    }
+
+    System.out.println(a[i]);
+    i++;
+    printArray(a, i, l);
+
+
+
+  }
+
+
+
+    public static void main(String[] args) {
+        int[] a={11,22,33,4,66,77};
+        printArray(a, 0, a.length-1);
+        
+    }
+    
+}
