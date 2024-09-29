0bafdcce3c03fa66a391b4de06dedea4d858c9d2
Pranet99 <praneethv65@gmail.com>
2024-09-29 21:33:47 +0530

    changes to recursion problem

--- Programms/Print_Array_Using_Recurssion.java
+++ Programms/Print_Array_Using_Recurssion.java
@@ -1,21 +1,27 @@
 public class Print_Array_Using_Recurssion {
-    public static int[] print_Array_Using_Recurssionprint(int[] a){
-        int i=0;
-        int l=a.length-1;
-        if(i<l){
+    public static int[] print_Array_Using_Recurssionprint(int[] a,int i,int l){
+        
+        int temp=0;
+        
+        if(i>= l){
             return a;
         }
+        temp=a[i];
         a[i]=a[l];
+        a[l]=temp;
         i++;
         l--;
-        return print_Array_Using_Recurssionprint(a);
+        return print_Array_Using_Recurssionprint(a,i,l);
         
 
     }
 
     public static void main(String[] args) {
-        int[] a={1,2,3,4,5};
-        System.out.println(print_Array_Using_Recurssionprint(a));
+        int[] a={0,0};
+        int[] arr=print_Array_Using_Recurssionprint(a,0,a.length-1);
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
         
     }
     
