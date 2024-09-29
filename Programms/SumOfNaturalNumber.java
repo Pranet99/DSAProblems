5c5f042c2765c5ac05e6c26c41a34a3d56c1f1f7
Pranet99 <praneethv65@gmail.com>
2024-09-29 22:08:41 +0530

    new program

---
+++ Programms/SumOfNaturalNumber.java
@@ -0,0 +1,17 @@
+public class SumOfNaturalNumber {
+    public static int sum(int N){
+      
+    
+            if(N==1){
+            return N;
+            }
+        
+        
+        return N +sum(N-1);
+
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+    
+}
